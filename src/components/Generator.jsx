19768f5ce4f578c062490e3038172597288d04b3
Ahmed Imtiaz Khan <Ahmed.imtiaz2000@gmail.com>
2024-11-05 05:44:40 +0500

    Added data for workouts and also added and styled buttons for different types of workouts

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
+import { WORKOUTS } from "../utils/swoldier";
 
 function Header(props) {
-  const { index, header, title } = props;
+  const { index, description, title } = props;
 
   return (
-    <div>
-      <div>
-        <p>{header}</p>
-        <h4>{title}</h4>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-center gap-2">
+        <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400 ">
+          {index}
+        </p>
+        <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
       </div>
+      <p className="text-sm sm:text-base mx-auto">{description}</p>
     </div>
   );
 }
@@ -17,15 +21,27 @@ function Header(props) {
 export default function Generator(props) {
   return (
     <SectionWrapper
-      header={"Generate your workout"}
-      title={["Lets get enormous"]}
+      id={"generate"}
+      header={"generate your workout"}
+      title={["It's", "Huge", "o'clock"]}
     >
       <Header
-        index={1}
-        header={"Generate your workout"}
-        title={"Lets get enormous"}
+        index={"01"}
+        title={"Pick your poison"}
+        description={"Select the workout you wish to endure."}
       />
-      asad
+      <div className="flex flex-col gap-4">
+        {Object.keys(WORKOUTS).map((type, typeindex) => {
+          return (
+            <button
+              className="bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg"
+              key={typeindex}
+            >
+              {type}
+            </button>
+          );
+        })}
+      </div>
     </SectionWrapper>
   );
 }
