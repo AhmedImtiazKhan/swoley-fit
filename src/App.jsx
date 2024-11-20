47ffc17e93d15b85168ba03cbde945fa4fdadc09
Ahmed Imtiaz Khan <Ahmed.imtiaz2000@gmail.com>
2024-11-20 07:42:34 +0500

    needed to uplift the stete so moved the state to be dfined at the component above

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,26 @@ import Workout from "./components/Workout";
 import Generator from "./components/Generator";
 import Hero from "./components/Hero";
 
-function App() {
+function App(props) {
+  const [workout, setWorkout] = useState(null);
+  const [muscles, setMuscles] = useState([]); // Initialize state for muscles
+  const [poison, setPoison] = useState("individual"); // Initialize state for poison
+  const [goal, setGoal] = useState("strength_power"); // Initialize state for goal
+  // const [workout, setWorkout] = useState(null); // Initialize workout state
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
-      <Generator />
-      <Workout />
+      <Generator
+        poison={poison}
+        setPoison={setPoison}
+        goal={goal}
+        setGoal={setGoal}
+        muscles={muscles}
+        setMuscles={setMuscles}
+      />
+      {workout && <Workout workout={workout} />}
     </main>
   );
 }
